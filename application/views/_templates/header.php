2534ce53e0c0ad8ecf55052bd2f0b8cc692d8acc
cschlue <schlue.charles@gmail.com>
2015-01-25 06:50:47 -0600

    Added filler content to home and added font awesome.

--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -12,9 +12,11 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <!-- Local CSS -->
-  <link rel="stylesheet" href="/public/styles/style.css">
+  <link rel="stylesheet" href="public/styles/style.css">
 
 </head>
   <body>
-    <div id='wrapper'>
+    <div id="wrapper">
--- application/views/index/index.php
+++ application/views/index/index.php
@@ -1,1 +1,14 @@
-It works some how!
+<div id="content">
+  <table class="table table-hover">
+
+    <?php
+      $i = 0;
+      while($i < 100){
+        $i++;
+        echo '<tr><td><i class="fa fa-tasks"></i></td><td>' . '$i is equal to ' . $i . '</tr></td>';
+      }
+
+    ?>
+
+  </table>
+</div>
